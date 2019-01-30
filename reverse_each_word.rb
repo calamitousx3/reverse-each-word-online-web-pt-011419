871555525e7ae74168fac3702c9a314ec49bd19e
Carmen Renee <carmengenpt@gmail.com>
2019-01-30 01:32:00 +0000

    Automatically backed up by Learn

--- reverse_each_word.rb
+++ reverse_each_word.rb
@@ -2,7 +2,7 @@ require "pry"
 
 def reverse_each_word(string)
   empty = []
-  string.split.map do |word|
+  string.split.collect do |word|
     empty << word.reverse
   end
   return empty.join(" ")
